0264ef52ad9558734ed89447f0dcd7d425deaf3d
Olena Lytvynenko <lena.litvinen4ik@gmail.com>
2025-09-04 13:59:09 +0300

    change local at atlas

--- client/index.js
+++ client/index.js
@@ -9,21 +9,21 @@ const addBtn = $('.form__btn');
     }
   
     // Збереження у localStorage
-    function saveTodos() {
-      const todos = [];
-      $('.todo-item').each(function () {
-        todos.push({
-          text: $(this).find('.todo-item__description').text(),
-          checked: $(this).hasClass('todo-item--checked')
-        });
-      });
-      localStorage.setItem('todos', JSON.stringify(todos));
-    }
+    // function saveTodos() {
+    //   const todos = [];
+    //   $('.todo-item').each(function () {
+    //     todos.push({
+    //       text: $(this).find('.todo-item__description').text(),
+    //       checked: $(this).hasClass('todo-item--checked')
+    //     });
+    //   });
+    //   localStorage.setItem('todos', JSON.stringify(todos));
+    // }
   
     // Створення елемента списку
     function createTodoEl(text, checked = false) {
       const $li = $('<li>', {
-        class: 'list-group-item todo-item' + (checked ? ' todo-item--checked' : '')
+        class: 'list-group-item todo-item' + (checked ? ' todo-item--checked' : ''), 'data-id': id
       });
   
       const $checkbox = $('<input>', { type: 'checkbox' }).prop('checked', checked);
@@ -38,40 +38,88 @@ const addBtn = $('.form__btn');
     }
   
     // Завантаження з localStorage
-    function loadTodos() {
+    // function loadTodos() {
+    //   todosUl.empty();
+    //   const todos = JSON.parse(localStorage.getItem('todos')) || [];
+    //   $.each(todos, function (_, todo) {
+    //     createTodoEl(todo.text, todo.checked);
+    //   });
+    // }
+    // loadTodos();
+    async function loadTodos() {
       todosUl.empty();
-      const todos = JSON.parse(localStorage.getItem('todos')) || [];
-      $.each(todos, function (_, todo) {
-        createTodoEl(todo.text, todo.checked);
+      const res = await fetch("/api/todo-list");
+      const todos = await res.json();
+      todos.forEach(todo => {
+        createTodoEl(todo.text, todo.isDone);
       });
     }
+    
     loadTodos();
+    
   
     // Додавання елемента
-    addBtn.on('click', function (e) {
+    // addBtn.on('click', function (e) {
+    //   e.preventDefault();
+    //   const text = $.trim(textInputUser.val());
+    //   if (!text) {
+    //     alert('Поле не може бути пустим!');
+    //     return;
+    //   }
+    //   createTodoEl(text);
+    //   saveTodos();
+    //   textInputUser.val('');
+    // });
+    addBtn.on('click', async function (e) {
       e.preventDefault();
       const text = $.trim(textInputUser.val());
-      if (!text) {
-        alert('Поле не може бути пустим!');
-        return;
-      }
-      createTodoEl(text);
-      saveTodos();
+      if (!text) { alert('Поле не може бути пустим!'); return; }
+    
+      const res = await fetch("/api/todo", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ text, isDone: false })
+      });
+      const newTodo = await res.json();
+      createTodoEl(newTodo.text, newTodo.isDone);
       textInputUser.val('');
     });
+    
   
     // видалення
-    todosUl.on('click', '.todo-item__delete', function () {
-      $(this).closest('.todo-item').remove();
-      saveTodos();
+    // todosUl.on('click', '.todo-item__delete', function () {
+    //   $(this).closest('.todo-item').remove();
+    //   saveTodos();
+    // });
+    todosUl.on('click', '.todo-item__delete', async function () {
+      const $li = $(this).closest('.todo-item');
+      const id = $li.data('id');
+      if (id) {
+        await fetch(`/api/todo/${id}`, { method: "DELETE" });
+      }
+      $li.remove();
     });
   
     // зміна чекбокса
-    todosUl.on('change', 'input[type="checkbox"]', function () {
-      $(this).closest('.todo-item').toggleClass('todo-item--checked', this.checked);
-      saveTodos();
+    // todosUl.on('change', 'input[type="checkbox"]', function () {
+    //   $(this).closest('.todo-item').toggleClass('todo-item--checked', this.checked);
+    //   saveTodos();
+    // });
+    todosUl.on('change', 'input[type="checkbox"]', async function () {
+      const $li = $(this).closest('.todo-item');
+      const checked = this.checked;
+      $li.toggleClass('todo-item--checked', checked);
+    
+      const id = $li.data('id'); // збережемо _id після створення
+      if (!id) return;
+    
+      await fetch(`/api/todo/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ isDone: checked })
+      });
     });
-  
+    
 
     if ($('#infoModal').length && typeof bootstrap !== 'undefined') {
       const infoModal = new bootstrap.Modal($('#infoModal')[0]);
